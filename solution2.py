af4af21ed6b804e7c2414085c708cd2b15bc49ba
jgohl-dci <johannes.gohl@dci-student.org>
2023-09-27 09:48:55 +0200

    update

--- solution2.py
+++ solution2.py
@@ -2,17 +2,17 @@
 test = 0
 
 months = {'January': 31,
-          'February': 28,
-          'March': 31,
-          'April': 30,
-          'May': 31,
-          'June': 30,
-          'July': 31,
-          'August': 31,
-          'September': 30,
-          'October': 31,
-          'November': 30,
-          'December': 31}
+        'February': 28,
+        'March': 31,
+        'April': 30,
+        'May': 31,
+        'June': 30,
+        'July': 31,
+        'August': 31,
+        'September': 30,
+        'October': 31,
+        'November': 30,
+        'December': 31}
 
 print('List of months: ')
 for key in months.keys():
@@ -24,7 +24,7 @@ user_input = user_input.capitalize()
 
 for compare in months.keys():
     if compare == user_input:
-        print('The month has', months[compare], 'days')
+        print('The month', compare, 'has', months[compare], 'days')
         test = 1
     else:
         continue
